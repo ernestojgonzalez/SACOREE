70e7703954d140126e4588c52e7f479a499d2f88
ernestojgonzalez <ernesto.jose.gonzalez.c@gmail.com>
2012-07-12 00:23:12 -0430

    comienzo con sacoree
    sistema yoelis

---
+++ insertar.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"[]>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
+<head>
+    <!--
+    Created by Artisteer v3.0.0.39952
+    Base template (without user's data) checked by http://validator.w3.org : "This page is valid XHTML 1.0 Transitional"
+    -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Inicio</title>
+    <meta name="description" content="Description" />
+    <meta name="keywords" content="Keywords" />
+
+
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+    <!--[if IE 6]><link rel="stylesheet" href="style.ie6.css" type="text/css" media="screen" /><![endif]-->
+    <!--[if IE 7]><link rel="stylesheet" href="style.ie7.css" type="text/css" media="screen" /><![endif]-->
+<link type="text/css" href="css/custom-theme/jquery-ui-1.8.21.custom.css" rel="stylesheet" />
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+	
+	
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.8.21.custom.min.js"></script>
+		<script type="text/javascript">
+			$(function(){
+
+				// Accordion
+				$("#accordion").accordion({ header: "h3" });
+
+				// Tabs
+				$('#tabs').tabs();
+
+				// Dialog
+				$('#dialog').dialog({
+					autoOpen: false,
+					width: 600,
+					buttons: {
+						"Ok": function() {
+							$(this).dialog("close");
+						},
+						"Cancel": function() {
+							$(this).dialog("close");
+						}
+					}
+				});
+
+				// Dialog Link
+				$('#dialog_link').click(function(){
+					$('#dialog').dialog('open');
+					return false;
+				});
+
+				// Datepicker
+				$('#datepicker').datepicker({
+					inline: true
+				});
+
+				// Slider
+				$('#slider').slider({
+					range: true,
+					values: [17, 67]
+				});
+
+				// Progressbar
+				$("#progressbar").progressbar({
+					value: 20
+				});
+
+				//hover states on the static widgets
+				$('#dialog_link, ul#icons li').hover(
+					function() { $(this).addClass('ui-state-hover'); },
+					function() { $(this).removeClass('ui-state-hover'); }
+				);
+
+			});
+		</script>
+		<style type="text/css">
+			/*demo page css*/
+			body{ font: 62.5% "Trebuchet MS", sans-serif; margin: 50px;}
+			.demoHeaders { margin-top: 2em; }
+			#dialog_link {padding: .4em 1em .4em 20px;text-decoration: none;position: relative;}
+			#dialog_link span.ui-icon {margin: 0 5px 0 0;position: absolute;left: .2em;top: 50%;margin-top: -8px;}
+			ul#icons {margin: 0; padding: 0;}
+			ul#icons li {margin: 2px; position: relative; padding: 4px 0; cursor: pointer; float: left;  list-style: none;}
+			ul#icons span.ui-icon {float: left; margin: 0 4px;}
+		</style>
+		
+</head>
+<body>
+<div id="art-page-background-middle-texture">
+<div id="art-main">
+    <div class="art-header">
+        <div class="art-header-clip">
+        <div class="art-header-center">
+            <div class="art-header-png"></div>
+        </div>
+        </div>
+    <div class="art-header-wrapper">
+    <div class="art-header-inner">
+        <div class="art-headerobject"></div>
+        <div class="art-logo">
+                 <h1 class="art-logo-name"><a href="./index.html">...SACOREE...</a></h1>
+                         <h2 class="art-logo-text">Nuestro deber es servir.</h2>
+                </div>
+    </div>
+    </div>
+    </div>
+    <div class="cleared reset-box"></div>
+<div class="art-nav">
+	<div class="art-nav-l"></div>
+	<div class="art-nav-r"></div>
+<div class="art-nav-outer">
+<div class="art-nav-wrapper">
+
+<?php include "menu.php" ?>
+
+
+</div>
+</div>
+</div>
+<div class="cleared reset-box"></div>
+<div class="art-sheet">
+        <div class="art-sheet-tl"></div>
+        <div class="art-sheet-tr"></div>
+        <div class="art-sheet-bl"></div>
+        <div class="art-sheet-br"></div>
+        <div class="art-sheet-tc"></div>
+        <div class="art-sheet-bc"></div>
+        <div class="art-sheet-cl"></div>
+        <div class="art-sheet-cr"></div>
+        <div class="art-sheet-cc"></div>
+        <div class="art-sheet-body">
+            <div class="art-content-layout">
+                <div class="art-content-layout-row">
+                    <div class="art-layout-cell art-content">
+<div class="art-post">
+    <div class="art-post-body">
+<div class="art-post-inner art-article">
+                                <div class="art-postmetadataheader">
+                                        <h2 class="art-postheader">
+                    <img src="./images/postheadericon.png" width="30" height="30" alt="" />Insertar
+                                        </h2>
+                    <div class="cleared"></div>
+                                    </div>
+                                <div class="art-postcontent">
+<!-- Accordion -->
+	
+		
+			
+		<div id="accordion">
+			<div>
+				<h3><a href="#">OPCIONES</a></h3>
+				<div>Al seleccionar el una opcion podra insertar distinta informacion</div>
+			</div>
+			<div>
+				<h3><a href="#">INSERTA EMPLEADO</a></h3>
+				<div>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</div>
+			</div>
+			<div>
+				<h3><a href="#">INSERTA EQUIPO</a></h3>
+				<div>Phasellus mattis tincidunt nibh.</div>
+			</div>
+			<div>
+				<h3><a href="#">INSERTA PIEZAS</a></h3>
+				<div>Nam dui erat, auctor a, dignissim quis.</div>
+			</div>
+			<div>
+				<h3><a href="#">CREAR ODT</a></h3>
+				<div>Nam dui erat, auctor a, dignissim quis.</div>
+			</div>
+		</div>
+		
+
+
+                </div>
+                <div class="cleared"></div>
+                </div>
+
+		<div class="cleared"></div>
+    </div>
+</div>
+
+                      <div class="cleared"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="cleared"></div>
+            <div class="art-footer">
+                <div class="art-footer-t"></div>
+                <div class="art-footer-l"></div>
+                <div class="art-footer-b"></div>
+                <div class="art-footer-r"></div>
+                <div class="art-footer-body">
+                            <div class="art-footer-text">
+                                
+                                                            </div>
+                    <div class="cleared"></div>
+                </div>
+            </div>
+    		<div class="cleared"></div>
+        </div>
+    </div>
+    <div class="cleared"></div>
+    <p class="art-page-footer"></p>
+</div>
+    <div class="cleared"></div>
+</div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<div class="art-nav-inner">
+	<ul class="art-hmenu">
+		<li>
+			<a href="./new-page.html" class="active"><span class="l"></span><span class="r"></span><span class="t">Insertar</span></a>
+		</li>	
+		<li>
+			<a href="./new-page-2.html"><span class="l"></span><span class="r"></span><span class="t">Consultar</span></a>
+		</li>
+		<li>
+			<a href="./new-page-2.html"><span class="l"></span><span class="r"></span><span class="t">Modificar</span></a>
+		</li>
+		<li>
+			<a href="./new-page-2.html"><span class="l"></span><span class="r"></span><span class="t">Eliminar</span></a>
+		</li>	
+	</ul>
+</div>
